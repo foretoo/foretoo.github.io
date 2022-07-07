07f04279ca55fbe20d43737b21c2279280064309
Sergey Borovikov <serg.there@gmail.com>
2022-07-07 19:50:07 +0300

    clear console on hmr

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,11 @@ import { render } from "preact"
 import { App } from "./App"
 import "./index.sass"
 
+if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    () => console.clear()
+  );
+}
+
 render(<App />, document.getElementById("root")!)
