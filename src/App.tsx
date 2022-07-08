ebdaa8b23491cd709a0400c506cb7590ce3e2737
Sergey Borovikov <serg.there@gmail.com>
2022-07-08 21:00:04 +0300

    add pointermove listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
+import { animate } from "helpers/animate"
+import { useEffect } from "preact/hooks"
 import { BBBL } from "./components/bbbl"
 
+const pivot = {
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2
+}
+const pointer = {
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2
+}
+const moveListener = (e: PointerEvent) => {
+  pointer.x = e.offsetX
+  pointer.y = e.offsetY
+}
+
 export const App = () => {
+
+  useEffect(() => {
+    animate({
+      loop: true,
+      ontick: () => {
+        pivot.x += (pointer.x - pivot.x) * 0.05
+        pivot.y += (pointer.y - pivot.y) * 0.05
+      }
+    }).play()
+    window.addEventListener("pointermove", moveListener)
+    return () => window.removeEventListener("pointermove", moveListener)
+  })
 
   return (
     <>
       <BBBL
         size={640}
         num={7}
-        pivot={{
-          x: window.innerWidth / 2,
-          y: window.innerHeight / 2
-        }}
+        pivot={pivot}
       />
     </>
   )
